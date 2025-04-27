f3d609af48e469cb4ae0a04504a23ea719839b81
jiyue333 <z2017ss@outlook.com>
2025-04-27 16:42:49 +0800

    update todo list

--- lab2/include/front/semantic.h
+++ lab2/include/front/semantic.h
@@ -47,6 +47,7 @@ map<std::string,ir::Function*>* get_lib_funcs();
 struct SymbolTable{
     vector<ScopeInfo> scope_stack;
     map<std::string,ir::Function*> functions;
+    int block_cnt;
 
     /**
      * @brief enter a new scope, record the infomation in scope stacks
@@ -107,8 +108,199 @@ struct Analyzer {
     // reject copy & assignment
     Analyzer(const Analyzer&) = delete;
     Analyzer& operator=(const Analyzer&) = delete;
+
+    // analysis functions
+    void analysisCompUnit(CompUnit*, ir::Program&);
+    void analysisFuncDef(FuncDef*, ir::Function&);
+    void analysisDecl(Decl*, vector<ir::Instruction*>&);
+    void analysisConstDecl(ConstDecl*, vector<ir::Instruction*>&);
+    void analysisBType(BType*, vector<ir::Instruction*>&);
+    void analysisConstDef(ConstDef*, vector<ir::Instruction*>&);
+    void analysisConstInitVal(ConstInitVal*, vector<ir::Instruction*>&);
+    void analysisVarDecl(VarDecl*, vector<ir::Instruction*>&);
+    void analysisVarDef(VarDef*, vector<ir::Instruction*>&);
+    void analysisInitVal(InitVal*, vector<ir::Instruction*>&);
+    void analysisFuncType(FuncType*, vector<ir::Instruction*>&);
+    void analysisFuncFParam(FuncFParam*, vector<ir::Instruction*>&);
+    void analysisFuncFParams(FuncFParams*, vector<ir::Instruction*>&);
+    void analysisBlock(Block*, ir::Function&);
+    void analysisBlockItem(BlockItem*, vector<ir::Instruction*>&);
+    void analysisStmt(Stmt*, vector<ir::Instruction*>&);
+    void analysisExp(Exp*, vector<ir::Instruction*>&);
+    void analysisCond(Cond*, vector<ir::Instruction*>&);
+    void analysisLVal(LVal*, vector<ir::Instruction*>&);
+    void analysisNumber(Number*, vector<ir::Instruction*>&);
+    void analysisPrimaryExp(PrimaryExp*, vector<ir::Instruction*>&);
+    void analysisUnaryExp(UnaryExp*, vector<ir::Instruction*>&);
+    void analysisUnaryOp(UnaryOp*, vector<ir::Instruction*>&);
+    void analysisFuncRParams(FuncRParams*, vector<ir::Instruction*>&);
+    void analysisMulExp(MulExp*, vector<ir::Instruction*>&);
+    void analysisAddExp(AddExp*, vector<ir::Instruction*>&);
+    void analysisRelExp(RelExp*, vector<ir::Instruction*>&);
+    void analysisEqExp(EqExp*, vector<ir::Instruction*>&);
+    void analysisLAndExp(LAndExp*, vector<ir::Instruction*>&);
+    void analysisLOrExp(LOrExp*, vector<ir::Instruction*>&);
+    void analysisConstExp(ConstExp*, vector<ir::Instruction*>&);
 };
 
 } // namespace frontend
 
 #endif
+
+
+/*
+// 编译单元 
+CompUnit -> (Decl | FuncDef) [CompUnit]
+
+// 声明 
+Decl -> ConstDecl | VarDecl
+
+// 常量声明 
+ConstDecl -> 'const' BType ConstDef { ',' ConstDef } ';'
+    ConstDecl.t  // 类型
+
+// 基本类型 
+BType -> 'int' | 'float'
+    BType.t      // 类型
+
+// 常量定义 
+ConstDef -> Ident { '[' ConstExp ']' } '=' ConstInitVal
+    ConstDef.arr_name // 数组名 (如果存在)
+
+// 常量初始值 
+ConstInitVal -> ConstExp | '{' [ ConstInitVal { ',' ConstInitVal } ] '}'
+    ConstInitVal.v // 值
+    ConstInitVal.t // 类型
+
+// 变量声明 
+VarDecl -> BType VarDef { ',' VarDef } ';'
+    VarDecl.t    // 类型
+
+// 变量定义 
+VarDef -> Ident { '[' ConstExp ']' } [ '=' InitVal ]
+    VarDef.arr_name // 数组名 (如果存在)
+
+// 初始值 
+InitVal -> Exp | '{' [ InitVal { ',' InitVal } ] '}'
+    InitVal.is_computable // 是否在编译时可计算
+    InitVal.v           // 值
+    InitVal.t           // 类型
+
+// 函数定义 
+FuncDef -> FuncType Ident '(' [FuncFParams] ')' Block
+    FuncDef.t // 返回类型
+    FuncDef.n // 函数名
+
+// 函数类型 
+FuncType -> 'void' | 'int' | 'float'
+
+// 函数形式参数 
+FuncFParam -> BType Ident ['[' ']' { '[' Exp ']' }]
+
+// 函数形式参数列表 
+FuncFParams -> FuncFParam { ',' FuncFParam }
+
+// 语句块 
+Block -> '{' { BlockItem } '}'
+
+// 块内项 
+BlockItem -> Decl | Stmt
+
+// 语句 
+Stmt -> LVal '=' Exp ';'
+      | Block
+      | 'if' '(' Cond ')' Stmt [ 'else' Stmt ]
+      | 'while' '(' Cond ')' Stmt
+      | 'break' ';'
+      | 'continue' ';'
+      | ';'
+      | 'return' [Exp] ';'
+      | [Exp] ';'
+
+// 表达式 
+Exp -> AddExp
+    Exp.is_computable // 是否在编译时可计算
+    Exp.v           // 值
+    Exp.t           // 类型
+
+// 条件表达式 
+Cond -> LOrExp
+    Cond.is_computable // 是否在编译时可计算
+    Cond.v           // 值
+    Cond.t           // 类型
+
+// 左值 
+LVal -> Ident {'[' Exp ']'}
+    LVal.is_computable // 是否在编译时可计算 (通常为 false)
+    LVal.v           // 值
+    LVal.t           // 类型
+    LVal.i           // 标识符 (变量名或数组名)
+
+// 数字 
+Number -> IntConst | floatConst
+
+// 初等表达式 
+PrimaryExp -> '(' Exp ')'
+            | LVal
+            | Number
+    PrimaryExp.is_computable // 是否在编译时可计算
+    PrimaryExp.v           // 值
+    PrimaryExp.t           // 类型
+
+// 一元表达式 
+UnaryExp -> PrimaryExp
+            | Ident '(' [FuncRParams] ')' // 函数调用
+            | UnaryOp UnaryExp
+    UnaryExp.is_computable // 是否在编译时可计算
+    UnaryExp.v           // 值
+    UnaryExp.t           // 类型
+
+// 一元运算符 
+UnaryOp -> '+' | '-' | '!'
+
+// 函数实参列表 
+FuncRParams -> Exp { ',' Exp }
+
+// 乘法表达式 
+MulExp -> UnaryExp { ('*' | '/' | '%') UnaryExp }
+    MulExp.is_computable // 是否在编译时可计算
+    MulExp.v           // 值
+    MulExp.t           // 类型
+
+// 加法表达式 
+AddExp -> MulExp { ('+' | '-') MulExp }
+    AddExp.is_computable // 是否在编译时可计算
+    AddExp.v           // 值
+    AddExp.t           // 类型
+
+// 关系表达式 
+RelExp -> AddExp { ('<' | '>' | '<=' | '>=') AddExp }
+    RelExp.is_computable // 是否在编译时可计算
+    RelExp.v           // 值
+    RelExp.t           // 类型
+
+// 相等性表达式 
+EqExp -> RelExp { ('==' | '!=') RelExp }
+    EqExp.is_computable // 是否在编译时可计算
+    EqExp.v           // 值
+    EqExp.t           // 类型
+
+// 逻辑与表达式 
+LAndExp -> EqExp [ '&&' LAndExp ]
+    LAndExp.is_computable // 是否在编译时可计算
+    LAndExp.v           // 值
+    LAndExp.t           // 类型
+
+// 逻辑或表达式 
+LOrExp -> LAndExp [ '||' LOrExp ]
+    LOrExp.is_computable // 是否在编译时可计算
+    LOrExp.v           // 值
+    LOrExp.t           // 类型
+
+// 常量表达式 
+ConstExp -> AddExp
+    ConstExp.is_computable: true // 必须在编译时可计算
+    ConstExp.v           // 值
+    ConstExp.t           // 类型
+
+*/
--- lab2/src/front/semantic.cpp
+++ lab2/src/front/semantic.cpp
@@ -30,28 +30,117 @@ map<std::string,ir::Function*>* frontend::get_lib_funcs() {
 }
 
 void frontend::SymbolTable::add_scope(Block* node) {
-    TODO;
+    ScopeInfo scope_info;
+    scope_info.name = 'b';
+    scope_info.cnt = ++block_cnt;
+    scope_stack.push_back(scope_info);
 }
 void frontend::SymbolTable::exit_scope() {
-    TODO;
+    --block_cnt;
+    scope_stack.pop_back();
 }
 
 string frontend::SymbolTable::get_scoped_name(string id) const {
-    TODO;
+    return id + '_' + std::to_string(block_cnt);
 }
 
 Operand frontend::SymbolTable::get_operand(string id) const {
-    TODO;
+    for (size_t i = scope_stack.size() - 1; i >= 0; i--)
+    {
+        map<string, STE> table = scope_stack[i].table;
+        if (table.count(id)) {
+            return table[id].operand;
+        }
+    }
+    return Operand(id, Type::Int);
 }
 
 frontend::STE frontend::SymbolTable::get_ste(string id) const {
-    TODO;
+    for (size_t i = scope_stack.size() - 1; i >= 0; i--)
+    {
+        map<string, STE> table = scope_stack[i].table;
+        if (table.count(id)) {
+            return table[id];
+        }
+    }
+    return STE();
 }
 
 frontend::Analyzer::Analyzer(): tmp_cnt(0), symbol_table() {
-    TODO;
+    ScopeInfo inittable;
+    inittable.cnt = 0;
+    inittable.name = 'global';
+    inittable.table = map_str_ste();
+    symbol_table.scope_stack.push_back(inittable);
 }
 
 ir::Program frontend::Analyzer::get_ir_program(CompUnit* root) {
+    TODO;
+}
+
+void frontend::Analyzer::analysisCompUnit(CompUnit*, ir::Program&) {
+    TODO;
+}
+void frontend::Analyzer::analysisFuncDef(FuncDef*, ir::Function&) {
+    TODO;
+}
+void frontend::Analyzer::analysisDecl(Decl*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisConstDecl(ConstDecl*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisBType(BType*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisVarDecl(VarDecl*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisVarDef(VarDef*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisInitVal(InitVal*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisFuncType(FuncType*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisFuncFParam(FuncFParam*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisBlockItem(BlockItem*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisStmt(Stmt*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisExp(Exp*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisCond(Cond*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisPrimaryExp(PrimaryExp*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisUnaryExp(UnaryExp*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisUnaryOp(UnaryOp*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisFuncRParams(FuncRParams*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisRelExp(RelExp*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisEqExp(EqExp*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisLAndExp(LAndExp*, vector<ir::Instruction*>&) {
+    TODO;
+}
+void frontend::Analyzer::analysisLOrExp(LOrExp*, vector<ir::Instruction*>&) {
     TODO;
 }
